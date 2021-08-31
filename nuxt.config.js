d4266dbe1a08ea97a92d4e6090cfbe5104228474
Kashif <kashif.andpercent@gmail.com>
2021-08-31 23:03:50 +0500

    code pushed

---
+++ nuxt.config.js
@@ -0,0 +1,63 @@
+import { resolve } from 'path'
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'trello-clone',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  alias: {
+    'images': resolve(__dirname, './assets/images'),
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    '@nuxtjs/tailwindcss'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: "AIzaSyBFdP7aXfyTNpiqewglEye78VonrVO3U-Q",
+          authDomain: "trello-clone-nuxt.firebaseapp.com",
+          projectId: "trello-clone-nuxt",
+          storageBucket: "trello-clone-nuxt.appspot.com",
+          messagingSenderId: "834143514212",
+          appId: "1:834143514212:web:9d7623b1dcf46dcaf0b971"
+        },
+        services: {
+          auth: true // Just as example. Can be any other service.
+        }
+      }
+    ]
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+}
---
+++ pages/auth/login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container flex justify-center items-center h-screen bg-gray-100">
+        <div class="w-1/4 bg-white  rounded-2xl shadow-md">
+            <div class="w-40 mx-auto">
+                <img src="~/images/Trello-logo.png" alt="">
+              
+            </div>
+            <div class="p-5 text-center">
+                 <input type="text" placeholder="Email" class="focus:ring-0 w-full rounded-2xl shadow-inner bg-gray-100 border-0">
+                  <input type="text" placeholder="Password" class=" focus:ring-0 w-full mt-3 rounded-2xl shadow-inner bg-gray-100 border-0">
+                <button class="bg-gradient-to-r from-blue-400 to-blue-800 mt-3 rounded-2xl
+                text-white font-medium shadow-inner bg-gray-100 border-0 w-1/2 h-10 mx-auto">login</button>
+                <div class="font-medium mt-3 ">
+                   <span class="text-gray-500"> Not a member?</span> <span class="text-blue-600">Signup now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    layout : 'auth'
+}
+</script>
+<style scoped>
+
+</style>
